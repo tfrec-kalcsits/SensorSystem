9ad957b7a3172d587f55fb8a009a4c64092a30ab
Ryan Mullin <ryan.mullin@wsu.edu>
2018-07-20 16:17:35 -0700

    Fixed outdated cmake files

---
+++ cmake/systemdependencies.cmake
@@ -0,0 +1,14 @@
+#This file fetches system dependencies for the projects. This includes:
+# - wiringPi
+# - libcurl
+# - libncurses
+
+find_package(CURL REQUIRED)
+
+set(CURSES_NEED_NCURSES TRUE)
+find_package(Curses REQUIRED)
+
+set(THREADS_PREFER_PTHREAD_FLAG ON)
+find_package(Threads REQUIRED)
+
+find_library(wiringPi wiringPi)
