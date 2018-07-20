7dc94b196d7dccb93e09761293de9ea55cd4e7e5
Ryan Mullin <ryan.mullin@wsu.edu>
2018-07-20 15:03:11 -0700

    restored lost headless sketch

--- SensorArduinoHeadless/SensorArduinoHeadless.ino
+++ SensorArduinoHeadless/SensorArduinoHeadless.ino
@@ -7,10 +7,12 @@
 #include <packet.h>
 #include <rf24radiotransmitter.h>
 
-//EEPROM library to be used for reading and writing persistent data
-#include <EEPROM.h>
-
 using namespace sensorsystem;
+
+#define MILLI_PER_MINUTE 60000.0f 
+
+//interval in minutes that the sensor will make a recording
+float log_interval = 0.5f;
 
 //ce and csn pins for the RF24. Set these to wherever the pins are plugged in on the board
 uint16_t ce_pin = 7;
@@ -18,7 +20,7 @@ uint16_t csn_pin = 8;
 
 //Write pipe address for RF24. Set this to one of the six read pipes of the Hub receiver radio
 //Change first byte to a number from 1-6 to change the pipe
-byte pipe[6];
+byte pipe[6] = "1Node";
 
 RadioTransmitter * radio;
 
@@ -31,8 +33,44 @@ TSLSensor light_sensor;
 //Each sensor can specify a signature to be recorded with measurements.
 //The signature is a C string of up to size 10.
 //Reset this line to set a custom signature.
-char signature[10];
+char signature[10] = "ARDUINO";
 
 void setup() {
-  Serial
+  //temp_sensor.begin() and light_sensor.begin() need to be called in order to initialize I2C communication
+  radio = new RF24RadioTransmitter(7, 8, pipe); 
+  temp_sensor.begin();
+  light_sensor.begin();
+
+  //Initialize Serial communication. This will be used for debugging purposes.
+  Serial.begin(9600);
+  Serial.print("beginning");
+}
+
+void loop() {
+  //For each record interval, we'll make a log request to the Hub
+  float ambient = temp_sensor.getAmbientTemperature();
+  float object = temp_sensor.getObjectTemperature();
+  float lux = light_sensor.getLux();
+
+  Packet packet;
+  packet.ambient_temperature = ambient;
+  packet.object_temperature = object;
+  packet.flag = Packet::Flag::LOG;
+  packet.lux = lux;
+  int i;
+  for(i = 0; i < 10 && signature[i] != '\0'; i++)
+    packet.signature[i] = signature[i];
+  packet.signature[i] = '\0';
+
+  bool success = radio->sendPacket(packet);
+
+  //print data for debugging purposes
+  Serial.println("===============================");
+  Serial.print("SENT: "); Serial.println(success ? "success" : "failed");
+  Serial.print("OBJECT: "); Serial.println(object);
+  Serial.print("AMBIENT: "); Serial.println(ambient);
+  Serial.print("LUX: "); Serial.println(lux);
+  
+  //wait for the interval time to make another request
+  delay((int)(log_interval * MILLI_PER_MINUTE));
 }
